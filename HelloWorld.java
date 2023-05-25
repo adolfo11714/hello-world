de68e218d6273d4f07ca0c09774b02fd91ecb467
adolfo11714 <adolfomartinez122@yahoo.com>
2023-05-25 12:30:48 -0400

    Revert "updated hello world"
    This reverts commit 0d39bae8988776710b0dbb3801032c56dc09d7e0.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,5 @@
 public class HelloWorld {
    public static void main(String[] args) {
       System.out.println("Hello World!");
-      
-      //name is a reference variable
-      String name = "George Washington";
-      
-      System.out.println(name);
-      
-      //age is a primitive variable
-      int age = 24;
-      
-      System.out.println(age);
    }
 }
